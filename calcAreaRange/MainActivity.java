9cdb10c268da7dc5da17476a7ab2204fa6ace53f
Adriel Ajala <100488130+adrielajala@users.noreply.github.com>
2022-09-21 07:58:57 -0300

    Update MainActivity.java

--- calcAreaRange/MainActivity.java
+++ calcAreaRange/MainActivity.java
@@ -17,26 +17,11 @@ public class MainActivity extends AppCompatActivity {
         calc();
     }
 
-    public void calc()
-    {
-        /*final Button button = (Button) findViewById(R.id.b_calc);
-        button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                double height = Double.parseDouble(((EditText) findViewById(R.id.e_height)).getText().toString());
-                double width = Double.parseDouble(((EditText) findViewById(R.id.e_width)).getText().toString());
-                TextView area = (TextView) findViewById(R.id.area_text);
-                DecimalFormat df = new DecimalFormat("#.#");
-                double area_total = height * width;
-                String s = "Área = " + Double.parseDouble(df.format(height * width)) + " u²";
-                area.setText(s);
-
-            }
-        });*/
+    public void calc() {
         final SeekBar s_width = (SeekBar) findViewById(R.id.w_slide);
         final SeekBar s_height = (SeekBar) findViewById(R.id.h_slide);
         final TextView area_view = (TextView) findViewById(R.id.area_text);
-        SeekBar.OnSeekBarChangeListener listener = new SeekBar.OnSeekBarChangeListener(){
+        SeekBar.OnSeekBarChangeListener listener = new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
                 int width = (int) s_width.getProgress();
