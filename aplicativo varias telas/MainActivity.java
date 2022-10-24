fa5083ef7de796365a07449fe28bc2d16c2df1ce
Adriel Ajala <100488130+adrielajala@users.noreply.github.com>
2022-10-24 14:47:50 -0300

    Create MainActivity.java

---
+++ aplicativo varias telas/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.multiactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void openActivity(View view, Class myClass) {
+        Intent intent = new Intent(this, myClass);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity(view, tela2.class);
+            }
+
+        });
+    }
+
+
+}
